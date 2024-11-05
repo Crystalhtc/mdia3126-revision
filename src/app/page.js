77f445635bac1298e554209adf85157741b274ed
crystalhtc <crystalhtc01@gmail.com>
2024-11-04 16:59:30 -0800

    anime list api

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,108 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  //Todo
+  
+  // button that fetches
+  // container for button
+  // container for displaying content (empty, loading, fulfilled state)
+  // fetch content (handle and format content)
+  // loading state
+  // error handling
+  // styling
+  // breakpoints (mobile first)
+  // function to clear data
+
+  const [loading, setLoading] = useState(false);
+  const [imageData, setImageData] = useState(null);
+
+  async function fetchImages() {
+    // console.log("button clicked");
+    // alert("button clicked");
+
+    if (imageData) {
+      // Clear the data if images are already loaded
+      setImageData(null);
+      return;
+    }
+    
+    const randomPage = Math.floor(Math.random() * 50) + 1;
+    const API_URL = `https://api.jikan.moe/v4/anime?page=${randomPage}&limit=5`;
+    setLoading(true);
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    console.log(data);
+    // const debuggerVar = "test var";
+    // const moreDebuggerVar = "another test var";
+    setImageData(data.data);
+    setLoading(false);
+
+  }
+
+  const Header = () => {
+    return (
+      <section>
+        <h1>Midterm App</h1>
+        <button 
+          className="border-2 border-black p-2" 
+          onClick={fetchImages}>
+            Fetch 📸
+        </button>
+      </section>
+    )
+  }
+
+  const ImageListContainer =() => {
+
+    if (loading) {
+      return (
+        <section>loading...</section>
+      )
+    }
+
+    if(imageData) {
+      const imageListItems = [];
+      // the image data should be an array of objects
+      // the forEach gives an error -> check if the data is an array
+      imageData.forEach((image,i) => {
+        // author: 
+        // "Alejandro Escamilla"
+        // download_url: 
+        // "https://picsum.photos/id/0/5000/3333"
+        // height: 
+        // 3333
+        // id: 
+        // "0"
+        // url: 
+        // "https://unsplash.com/photos/yC-Yzbqy7PY"
+        // width: 
+        // 5000`
+
+          imageListItems.push(
+            <article key={image.id}>
+              <img src={image.images.jpg.image_url} width="200"/>
+              <p>Author: {image.title}</p>
+              <p>Duration: {image.duration}</p>
+              <hr/>
+            </article>
+          )
+      });
+      return <section className="flex flex-col  bg-teal-100" >{imageListItems}</section>;
+    }
+
+    return (
+      <section>no images fetched! ☹️</section>
+    )
+  }
+
+  return (
+    <div className="m-2">
+      <Header/>
+      <ImageListContainer/>
     </div>
   );
 }
